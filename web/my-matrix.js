a6a7241ac580bc4845d2f70b802e682e4647a878
James McNeill <mcneja@gmail.com>
2022-09-13 21:50:21 -0700

    Add some more matrix functions

--- web/my-matrix.js
+++ web/my-matrix.js
@@ -224,6 +224,108 @@ var mat4;
         result[15] = a[15];
     }
     mat4.copy = copy;
+    function identity(m) {
+        m.fill(0);
+        m[0] = 1;
+        m[5] = 1;
+        m[10] = 1;
+        m[15] = 1;
+    }
+    mat4.identity = identity;
+    function translate(m, v) {
+        // Pre-multiply m by translation matrix v
+        m[0] += m[3] * v[0];
+        m[1] += m[3] * v[1];
+        m[2] += m[3] * v[2];
+        m[4] += m[7] * v[0];
+        m[5] += m[7] * v[1];
+        m[6] += m[7] * v[2];
+        m[8] += m[11] * v[0];
+        m[9] += m[11] * v[1];
+        m[10] += m[11] * v[2];
+        m[12] += m[15] * v[0];
+        m[13] += m[15] * v[1];
+        m[14] += m[15] * v[2];
+    }
+    mat4.translate = translate;
+    function scale(m, s) {
+        // Pre-multiply by scale matrix
+        m[0] *= s[0];
+        m[1] *= s[1];
+        m[2] *= s[2];
+        m[4] *= s[0];
+        m[5] *= s[1];
+        m[6] *= s[2];
+        m[8] *= s[0];
+        m[9] *= s[1];
+        m[10] *= s[2];
+        m[12] *= s[0];
+        m[13] *= s[1];
+        m[14] *= s[2];
+    }
+    mat4.scale = scale;
+    function rotateX(m, angle) {
+        const s = Math.sin(angle);
+        const c = Math.cos(angle);
+        const m1 = m[1];
+        const m2 = m[2];
+        const m5 = m[5];
+        const m6 = m[6];
+        const m9 = m[9];
+        const m10 = m[10];
+        const m13 = m[13];
+        const m14 = m[14];
+        m[1] = m1 * c + m2 * s;
+        m[2] = m2 * c - m1 * s;
+        m[5] = m5 * c + m6 * s;
+        m[6] = m6 * c - m5 * s;
+        m[9] = m9 * c + m10 * s;
+        m[10] = m10 * c - m9 * s;
+        m[13] = m13 * c + m14 * s;
+        m[14] = m14 * c - m13 * s;
+    }
+    mat4.rotateX = rotateX;
+    function frustum(m, left, right, bottom, top, near, far) {
+        const x = (2 * near) / (right - left);
+        const y = (2 * near) / (top - bottom);
+        const a = (right + left) / (right - left);
+        const b = (top + bottom) / (top - bottom);
+        const c = (near + far) / (near - far);
+        const d = (2 * near * far) / (near - far);
+        const m0 = m[0];
+        const m1 = m[1];
+        const m2 = m[2];
+        const m3 = m[3];
+        const m4 = m[4];
+        const m5 = m[5];
+        const m6 = m[6];
+        const m7 = m[7];
+        const m8 = m[8];
+        const m9 = m[9];
+        const m10 = m[10];
+        const m11 = m[11];
+        const m12 = m[12];
+        const m13 = m[13];
+        const m14 = m[14];
+        const m15 = m[15];
+        m[0] = m0 * x + m2 * a;
+        m[1] = m1 * y + m2 * b;
+        m[2] = m2 * c + m3 * d;
+        m[3] = -m2;
+        m[4] = m4 * x + m6 * a;
+        m[5] = m5 * y + m6 * b;
+        m[6] = m6 * c + m7 * d;
+        m[7] = -m6;
+        m[8] = m8 * x + m10 * a;
+        m[9] = m9 * y + m10 * b;
+        m[10] = m10 * c + m11 * d;
+        m[11] = -m10;
+        m[12] = m12 * x + m14 * a;
+        m[13] = m13 * y + m14 * b;
+        m[14] = m14 * c + m15 * d;
+        m[15] = -m14;
+    }
+    mat4.frustum = frustum;
     function ortho(result, left, right, bottom, top, near, far) {
         const lr = 1 / (left - right);
         const bt = 1 / (bottom - top);
